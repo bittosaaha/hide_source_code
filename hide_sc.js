9825a528b4bc7a09928fe66cb20524e1fe187f50
bittosaaha <156645017+bittosaaha@users.noreply.github.com>
2024-02-21 18:42:37 +0600

    Update hide_sc.js

--- hide_sc.js
+++ hide_sc.js
@@ -3,6 +3,6 @@
 //disables Right Click/ Context Menu
 //disables Ctrl+I (For chrome)
 //disable eruda script injections
-//Enjoy spread good things :)
+//Enjoy, spread good things :)
 
 !function(){document['addEventListener']('keydown',function(e){if(e['ctrlKey']&&e['key']==='i'){e['preventDefault']();}});document['addEventListener']('contextmenu',function(e){e['preventDefault']();});var a=document['body']['append'];document['body']['append']=function(){for(var e=0;e<arguments['length'];e++){if(arguments[e]['tagName']&&arguments[e]['tagName']['toLowerCase']()==='script'){console['error']('Appending script tag to body is not allowed.');return;}}a['apply'](this,arguments);};}()
